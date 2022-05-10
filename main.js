2c34bcfcd57757d87fbeec8861b922ea85341651
Nyna Yusof <ngnaaa@gmail.com>
2022-05-10 11:42:02 +0800

    Added Function

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 const cursor = document.querySelector(".cursor");
+const holes = [...document.querySelectorAll(".hole")];
+const scoreEl = document.querySelector(".score span");
+let score = 0;
 
+function run() {
+  const i = Math.floor(Math.random() * holes.length);
+  const hole = holes[i];
+  let timer = null;
+
+  const img = document.createElement("img");
+  img.classList.add("mole");
+  img.src = "Media/mole.png";
+  hole.appendChild(img);
+
+  img.addEventListener("click", () => {
+    score += 10;
+    scoreEl.textContent = score;
+    img.src = "Media/mole-whacked.png";
+    clearTimeout(timer);
+    setTimeout(() => {
+      hole.removeChild(img);
+      run();
+    }, 500);
+  });
+
+  timer = setTimeout(() => {
+    hole.removeChild(img);
+    run();
+  }, 1000);
+}
+
+run();
+console.log(holes);
 window.addEventListener("mousemove", (e) => {
   cursor.style.top = e.pageY + "px";
   cursor.style.left = e.pageX + "px";
